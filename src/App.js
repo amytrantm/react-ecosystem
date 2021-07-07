6340c84cdc9bbc06bceabe84b407dbbe0ddb5a8e
Amy  Tran <trantm.amy@gmail.com>
2021-07-07 15:48:18 -0700

    create and remove todo features

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
-import TodoList from './todo/TodoList';
+import TodoList from './todos/TodoList';
 import './App.css';
 
 const App = () => (
